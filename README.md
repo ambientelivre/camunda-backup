3fca3a69417100a34a0b6150da65cffe3e95d767
PedroKravetz <p.v.kravetz@gmail.com>
2023-08-29 16:11:42 -0300

    Script atualizado

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
 <h1>Backup Camunda</h1>
-Script desenvolvido para facilitar processo de backup do software Camunda, o script atualmente recupera dados do banco de dados (Postgres/MariaDB/Mysql), e também recupera os Jars customizados adicionados pelo usuário.
+Script desenvolvido para facilitar processo de backup do software Camunda, o script atualmente recupera dados do banco de dados (Postgres/MariaDB/Mysql), e também recupera os arquivos customizados adicionados pelo usuário.
 
 <h2>Informações importantes</h2>
-O script te da a possibilidade de realizar backups incrementais do Jars utilizados no Camunda. No entanto, o backup do Jars é realizado recuperando os arquivos criados por meio de "Volumes" específicos. Ele não pegará diretamente os arquivos do Container. Para que o script funcione corretamente, é necessário criar volumes para a pasta "lib" do Camunda.
-
-<h2>Utilizando script de volumes </h2>
-Antes de utilizar suba o container do Camunda, após isso execute o script com o comando:
-./create_volumes.sh
+Atualmente, temos a opção de script para as versões docker do camunda. Considerando isso, não é uma boa prática colocar baixar as pastas do container e colocar elas no volumes no docker-compose, pq qualquer atualização de versão do camunda ocasionará problemas. O recomendado é pegar arquivo por arquivo e adicionar no docker-compose.yml.
 
 <h2>Pré-requisitos</h2>
-Antes de executar o script, certifique-se de que você tenha instalado o Docker em sua máquina e que o ambiente "Camunda run" esteja  configurado.
+Antes de executar o script, certifique-se de que você tenha instalado o Docker em sua máquina e que o ambiente "Camunda Run" ou "Camunda Tomcat" nas versões docker estejam configurados, assim como o banco de dados Mysql/Postgres.
 
 <h2>Como utilizar o script</h2>
 
-- Primeiro, certifique-se de ter criado os volumes necessários para a pasta "lib" do Camunda:
+- Altere as variáveis de acordo com o seu ambiente (usuários, pastas, senhas, etc).
 
 - Agora, você pode executar o script de backup usando o seguinte comando:
 ```
--- backup-camunda-docker.sh
+++ backup-camunda-docker.sh
@@ -1,12 +1,11 @@
-## Script created by Marcos Churkin/marcos@ambientelivre.com.br
+## Script created by Marcos Churkin/marcos@ambientelivre.com.br and Pedro Kravetz
 
 
-DESTDIR=/opt/camunda/backup    ##Destino onde será feito o backup
-DATENOW=$(date +%d-%m-%y)      ##Nome setado para pasta
-LOCATE_CAMUNDA=/opt/camunda    ##Local de instalação Camunda
-WEB_INF=/opt/camunda/camunda/camunda-data/webapps/camunda/WEB-INF/lib ##Local reponsavel de armazenamento de JAR
-DBENGINE=postgres              ## Banco de dados para backup (postgres OU mariadb)
-BACKUP_INCREMENTAL=false        ## Ativa ou Desativa backup incremental (true OU false)
+DESTDIR=/opt/camunda/backup    ## Destino onde será feito o backup
+DATE_NOW=$(date +%y-%m-%d)     ## Nome setado para pasta
+LOCATE_CAMUNDA=/opt/camunda    ## Local de instalação Camunda
+DBENGINE=mysql                 ## Banco de dados para backup (postgres OU mariadb)
+DOCKER_VOLUMES=camunda         ## Pasta dentro do local de instalação do Camunda onde os arquivos de configuração são instalados
 
 ##Configuração Database Postgres
 PGUSER=camunda
@@ -27,39 +26,18 @@ DBDATABASE=process-engine
 mkdir $DESTDIR/$DATENOW
 cd $LOCATE_CAMUNDA
 
-if [ $DBENGINE = "mariadb" ]
+if [ $DBENGINE = "mysql" ]
 then
-  docker-compose exec mariadb mysqldump -u$DBUSER -p$DBPASS $DBDATABASE > $DESTDIR/$DATE_NOW/$DBDATABASE'_'$DBENGINE.sql
+  docker-compose exec mysql mysqldump -u$DBUSER -p$DBPASS $DBDATABASE > $DESTDIR/$DATE_NOW/$DBDATABASE'_'$DBENGINE.sql
 elif [ $DBENGINE = "postgres" ]
 then
   docker-compose exec postgres pg_dump --username $PGUSER $PGDATABASE > $DESTDIR/$DATE_NOW/postgresql.sql
 fi
 
+cp $LOCATE_CAMUNDA/docker-compose.yml  $DESTDIR/$DATE_NOW/
 
-if [ $BACKUP_INCREMENTAL = "false" ]
-then
-   tar -pczvf $DESTDIR/$DATENOW/WEB-INF_backup_full.tar.gz $WEB_INF
-   cp $LOCATE_CAMUNDA/docker-compose.yml $DESTDIR/$DATENOW
+tar -pczvf $DESTDIR/$DATE_NOW/camundaVolumes.tar.gz $DOCKER_VOLUMES
 
-#Script para realizar backup incremental
-elif [ $BACKUP_INCREMENTAL = "true" ]
-then
-if [ -f $DESTDIR/ultimo_backup_completo.txt ]
-then
-  ULTIMO_BACKUP_COMPLETO=$(cat $DESTDIR/ultimo_backup_completo.txt)
-else
-  ULTIMO_BACKUP_COMPLETO="nunca"
-fi
+##Localiza arquivos criados/modificados a mais de 365 dias e excluem
+find $DESTDIR* -mtime +365 -exec rm -rf {} \;
 
-if [ $ULTIMO_BACKUP_COMPLETO = "nunca" ]
-then
-  tar -pczvf $DESTDIR/$DATENOW/WEB-INF_backup_full.tar.gz $WEB_INF
-
-  #Cria arquivo para verificação de backup
-  touch $DESTDIR/ultimo_backup_completo.txt
-  echo  $DATENOW > $DESTDIR/ultimo_backup_completo.txt
-else
-  #Verifica ultima data de backup, e salva somente arquivos modificados após a data.
-  tar -pczvf $DESTDIR/$DATENOW/WEB-INF_backup_incremental.tar.gz --newer=$ULTIMO_BACKUP_COMPLETO $WEB_INF
-fi
-fi
